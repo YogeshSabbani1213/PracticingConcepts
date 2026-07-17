41df5abee0290f79b74cc43ed067b139e72c9aa6
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-07-17 18:41:49 +0530

    Added Login page for Oauth

---
+++ src/pages/Login.tsx
@@ -0,0 +1,26 @@
+import { GoogleLogin } from "@react-oauth/google";
+
+function Login() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-100 px-4">
+      <div className="w-full max-w-md rounded-xl bg-white p-8 shadow-lg">
+        <h1 className="mb-6 text-center text-3xl font-bold text-slate-800">
+          Login
+        </h1>
+
+        <div className="flex justify-center">
+          <GoogleLogin
+            onSuccess={(credentialResponse) => {
+              console.log(credentialResponse);
+            }}
+            onError={() => {
+              console.log("Login Failed");
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
